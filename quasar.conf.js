383c25e72dcd2fb40129594a627f81c0a76f2b52
dperezch <davarmando@gmail.com>
2023-01-28 20:38:15 -0300

    nueva versión, reparado error de icon y splash screen, en producción, publicado en playstore

--- quasar.conf.js
+++ quasar.conf.js
@@ -174,7 +174,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
     cordova: {
-      version: '3.0.4'
+      version: '3.0.6'
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
